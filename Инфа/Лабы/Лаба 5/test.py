268921b09a64f53a570e9b205dfc6bd02ff09cf5
Antichnoste <95621095+Antichnoste@users.noreply.github.com>
2024-12-12 16:06:39 +0300

    Add files via upload

---
+++ Инфа/Лабы/Лаба 5/test.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+df = pd.read_csv("data.csv")
+fig, ax = plt.subplots(1, 4,figsize=(40, 5))
+
+print(fig,ax)
+df.groupby(['<DATE>']).boxplot(column="<OPEN>,<HIGH>,<LOW>,<CLOSE>".split(","), ax=ax)
+plt.show()
